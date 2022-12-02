1329cf5ad58ad97ebd01928d90b99dd5e198d39b
Abhinav Sriram <abhinav_sriram@brown.edu>
2022-12-02 17:56:31 -0500

    some parts of it work

--- interface/server/server.js
+++ interface/server/server.js
@@ -2,33 +2,39 @@ let serialport = require("serialport");
 let SerialPort = serialport.SerialPort;
 
 // list serial ports:
-// serialport.list().then(
-//   (ports) => ports.forEach((port) => console.log(port.path)),
-//   (err) => console.log(err)
-// );
+serialport.list().then(
+  (ports) => ports.forEach((port) => console.log(port.path)),
+  (err) => console.log(err)
+);
+
 // get port name from the command line:
 // use as node server.js /dev/cu.usbmodem1411
 let portName = process.argv[2];
-const myPort = new SerialPort({
-  path: portName,
-  baudRate: 9600,
-  // parser: new Readline("\n")
-});
-// let Readline = SerialPort.parsers.Readline; // make instance of Readline parser
-// let parser = new Readline(); // make a new parser to read ASCII lines
-// myPort.pipe(parser); // pipe the serial stream to the parser
+const myPort = new serialport(portName, 9600);
+let Readline = serialport.parsers.Readline; // make instance of Readline parser
+let parser = new Readline(); // make a new parser to read ASCII lines
+myPort.pipe(parser); // pipe the serial stream to the parser
 
+// call showPortOpen when port opens
 myPort.on("open", showPortOpen);
-// parser.on("data", readSerialData);
+// call readSerialData while port is open
+parser.on("data", readSerialData);
+// call showPortClose when port closes
 myPort.on("close", showPortClose);
+// call showError when there is any error
 myPort.on("error", showError);
 
 function showPortOpen() {
   console.log("port open. Data rate: " + myPort.baudRate);
 }
 
+// reads data
+// prints Hello every 10s
 function readSerialData(data) {
   console.log(data);
+  //   setTimeout(() => {
+  //     myPort.write("Hello from the server");
+  //   }, 10000);
 }
 
 function showPortClose() {
