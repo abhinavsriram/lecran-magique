3837bfb8cc07d508df7f3ff3df64a2793f369b18
Daniel Ghebreyesus <daniel_ghebreyesus@brown.edu>
2022-12-02 14:30:06 -0500

    works with serialport

--- interface/server/server.js
+++ interface/server/server.js
@@ -2,35 +2,39 @@ let serialport = require("serialport");
 let SerialPort = serialport.SerialPort;
 
 // list serial ports:
-serialport.list().then(
-  (ports) => ports.forEach((port) => console.log(port.path)),
-  (err) => console.log(err)
-);
+// serialport.list().then(
+//   (ports) => ports.forEach((port) => console.log(port.path)),
+//   (err) => console.log(err)
+// );
 // get port name from the command line:
 // use as node server.js /dev/cu.usbmodem1411
-// let portName = process.argv[2];
-// let myPort = new SerialPort(portName, 9600);
+let portName = process.argv[2];
+const myPort = new SerialPort({
+  path: portName,
+  baudRate: 9600,
+  // parser: new Readline("\n")
+});
 // let Readline = SerialPort.parsers.Readline; // make instance of Readline parser
 // let parser = new Readline(); // make a new parser to read ASCII lines
 // myPort.pipe(parser); // pipe the serial stream to the parser
 
-// myPort.on("open", showPortOpen);
+myPort.on("open", showPortOpen);
 // parser.on("data", readSerialData);
-// myPort.on("close", showPortClose);
-// myPort.on("error", showError);
+myPort.on("close", showPortClose);
+myPort.on("error", showError);
 
-// function showPortOpen() {
-//   console.log("port open. Data rate: " + myPort.baudRate);
-// }
+function showPortOpen() {
+  console.log("port open. Data rate: " + myPort.baudRate);
+}
 
-// function readSerialData(data) {
-//   console.log(data);
-// }
+function readSerialData(data) {
+  console.log(data);
+}
 
-// function showPortClose() {
-//   console.log("port closed.");
-// }
+function showPortClose() {
+  console.log("port closed.");
+}
 
-// function showError(error) {
-//   console.log("Serial port error: " + error);
-// }
+function showError(error) {
+  console.log("Serial port error: " + error);
+}
