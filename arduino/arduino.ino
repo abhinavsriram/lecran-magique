834a28757de591cc1a2b310c987846b9ccd4cab8
Daniel Ghebreyesus <daniel_ghebreyesus@brown.edu>
2022-12-03 18:39:54 -0500

    testing arduino file

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,4 +1,4 @@
-int a = 0;
+int totalLinesProcessed = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -6,7 +6,47 @@ void setup() {
 
 // the loop routine runs over and over again forever:
 void loop() {
-  Serial.println(a);
-  a += 1;
-  delay(10000);  // delay in between reads for stability
+  // Serial.println(a);
+  // a += 1;
+  // delay(10000);  // delay in between reads for stability
+
+  String currLine;
+  while (Serial.available()) {
+    currLine += char(Serial.read());
+  }
+
+  // SD, SL <Int> <Int>, SR, AD <Int>, AR <Int>
+  if (currLine.charAt(0) == 'S') {
+    switch (currLine.charAt(1)) {
+      case 'D':
+        digitalWrite(6, HIGH);
+        delay(3000);
+        digitalWrite(6, LOW);
+        delay (3000);
+        break;
+      case 'L':
+      {
+        int spaceDelim1 = currLine.indexOf(" ");
+        int spaceDelim2 = currLine.lastIndexOf(" ");
+        int xCoord = currLine.substring(spaceDelim1 + 1, spaceDelim2).toInt();
+        int yCoord = currLine.substring(spaceDelim2 + 1).toInt();
+        totalLinesProcessed += 1;
+        // this will be in another function call...
+        Serial.println("AD " + totalLinesProcessed);
+        break;
+      }
+      case 'R':
+        for (int i = 0; i < 10; i++) {
+          digitalWrite(6, HIGH);
+          delay(1000);
+          digitalWrite(6, LOW);
+          delay (1000);
+          // this will be in another function call...
+          Serial.println("AR " + i);
+        }
+        break;
+      default:
+        break;
+    }
+  }
 }
